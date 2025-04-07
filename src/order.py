ff78f7485da7142ab3d73e11b40c7f2e1a67d8d9
Alexei <streiker.saik@gmail.com>
2025-04-07 13:51:39 +0300

    add htmlcov

--- src/order.py
+++ src/order.py
@@ -61,6 +61,6 @@ class Order(BaseEntity):
         self.total_price = self.quantity * self.product.price
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     Order(product1, 5)
